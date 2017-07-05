731befc1c9f44b17f55fb96394caa3d8bced8608
Kentaro Kawano <kentaro.kawano@gmail.com>
2017-07-05 13:01:35 +0900

    add test for modified time package

---
+++ timepkg/sample_test.go
@@ -0,0 +1,29 @@
+package sample
+
+import (
+	"testing"
+	"time"
+)
+
+func fakeHour(hour int) {
+	time.Slip(time.Date(2017, 7, 7, hour, 0, 0, 0, time.Local))
+}
+func TestCanDeliver(t *testing.T) {
+	cases := []struct {
+		hour int
+		want bool
+	}{
+		{7, false}, {8, true}, {20, true}, {21, false},
+	}
+
+	for _, c := range cases {
+		fakeHour(c.hour) // fakeHourで時刻を書き換える
+		t.Log(time.Now())
+		got := CanDeliver()
+		if got != c.want {
+			t.Errorf("hour: %d, CanDeliver() => %t, want %t", c.hour, got, c.want)
+		}
+	}
+
+	time.ResetFake() // reset time
+}
